c78b81a4375e62d4c0cb28f030943bf13bd3994b
Carlos Javier Bazan Huaman <pacoyx@hotmail.com>
2020-08-14 23:20:43 -0500

    commitInicial

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+import { NavbarComponent } from './navbar/navbar.component';
+import { GraficoBarraHorizontalComponent } from './grafico-barra-horizontal/grafico-barra-horizontal.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+
+@NgModule({
+  declarations: [
+    NavbarComponent,
+    GraficoBarraHorizontalComponent
+  ],
+  exports: [
+    NavbarComponent,
+    GraficoBarraHorizontalComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    BrowserAnimationsModule,
+    NgxChartsModule
+  ]
+})
+export class ComponentsModule { }
---
+++ src/app/components/grafico-barra-horizontal/grafico-barra-horizontal.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-grafico-barra-horizontal',
+  templateUrl: './grafico-barra-horizontal.component.html',
+  styleUrls: ['./grafico-barra-horizontal.component.css']
+})
+export class GraficoBarraHorizontalComponent {
+
+  results: any[];
+
+  view: any[] = [700, 400];
+
+  // options
+  showXAxis = true;
+  showYAxis = true;
+  gradient = true;
+  showLegend = true;
+  showXAxisLabel = true;
+  xAxisLabel = 'Juegos';
+  showYAxisLabel = true;
+  yAxisLabel = 'Votos';
+
+  colorScheme = 'nightLights';
+
+  constructor() {}
+
+  onSelect(event) {
+    console.log(event);
+  }
+
+}
