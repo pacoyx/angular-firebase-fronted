767f359b41eed2cb9fcc09dc84db388cd48dc88a
Carlos Javier Bazan Huaman <pacoyx@hotmail.com>
2020-08-17 09:23:41 -0500

    first commit

--- src/app/components/grafico-barra-horizontal/grafico-barra-horizontal.component.ts
+++ src/app/components/grafico-barra-horizontal/grafico-barra-horizontal.component.ts
@@ -1,15 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+// import { single } from './data';
 
 @Component({
   selector: 'app-grafico-barra-horizontal',
   templateUrl: './grafico-barra-horizontal.component.html',
   styleUrls: ['./grafico-barra-horizontal.component.css']
 })
-export class GraficoBarraHorizontalComponent {
+export class GraficoBarraHorizontalComponent implements OnDestroy {
 
-  results: any[];
+  results: any[] = [
+    {
+      'name': 'Juego 1',
+      'value': 20
+    },
+    {
+      'name': 'Juego 2',
+      'value': 34
+    },
+    {
+      'name': 'Juego 3',
+      'value': 16
+    },
+    {
+      'name': 'Juego 4',
+      'value': 29
+    }
+  ];;
 
-  view: any[] = [700, 400];
+
 
   // options
   showXAxis = true;
@@ -22,11 +42,31 @@ export class GraficoBarraHorizontalComponent {
   yAxisLabel = 'Votos';
 
   colorScheme = 'nightLights';
+  intervalo;
 
-  constructor() {}
+  constructor() {
+
+    this.intervalo = setInterval(() => {
+
+      console.log('tick');
+      const newResults = [...this.results];
+
+      for (let i in newResults) {
+        newResults[i].value = Math.round(Math.random() * 500);
+      }
+
+      this.results = [...newResults];
+
+    }, 1500);
+
+  }
 
   onSelect(event) {
     console.log(event);
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.intervalo);
   }
 
 }
